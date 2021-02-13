8583edd93474a241b28c617b6ade7c2b6ac23244
Sam Javner <samuel.javner@gmail.com>
2021-02-13 09:41:21 -0600

    Add code to start express server when deployed
    Suggested here:
    https://medium.com/@kristof.dsantos/deploy-a-nuxt-app-with-express-to-heroku-82c5890562ef
    https://codesandbox.io/s/github/nuxt-community/express-template/tree/demo?file=/api/index.js

--- api/index.ts
+++ api/index.ts
@@ -7,3 +7,12 @@ app.get("/hello", function (_, res) {
 });
 
 export default app;
+
+// Start standalone server if directly running
+if (require.main === module) {
+    const port = process.env.PORT || 3001;
+    app.listen(port, () => {
+        // eslint-disable-next-line no-console
+        console.log(`API server listening on port ${port}`);
+    });
+}
